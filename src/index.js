7863c7729c905ea545f50c723e8445f764ea8fbe
Sasha Vasylenko <svasilenko389@gmail.com>
2022-09-09 00:08:32 +0300

    .

--- src/index.js
+++ src/index.js
@@ -83,7 +83,7 @@ const rangeFilter = (from, to) => {
 const minPriceReducer = () => {
 
   return goods.reduce((accum, item) => {
-    if(item.price > accum) return  item.price;
+    if(item.price <= accum) return  item.price;
     else return accum;
   }, 0)
  };
